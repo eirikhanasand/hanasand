61fc3ad1947d156d054dd666ebf634c3e3646392
eirikhanasand <eirik.hanasand@gmail.com>
2025-05-12 06:35:54 +0200

    Removed irrelevant content

--- api/src/interfaces.ts
+++
@@ -1,8 +0,0 @@
-// JFrog Artifactory Worker Status
-export enum DownloadStatus {
-    DOWNLOAD_UNSPECIFIED = 0,
-    DOWNLOAD_PROCEED = 1,
-    DOWNLOAD_STOP = 2,
-    DOWNLOAD_WARN = 3,
-    UNRECOGNIZED = -1
-}
