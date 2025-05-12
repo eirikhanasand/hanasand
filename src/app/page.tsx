f628210524fc2cc844d89fcc09588caaf85a1c54
eirikhanasand <eirik.hanasand@gmail.com>
2025-05-12 06:37:26 +0200

    Added limits to prevent excessive article length

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function page() {
         <div className="h-full">
             <Content />
             <Featured />
-            <Articles />
+            <Articles includeAll={false} max={4} />
             <Apps />
             <Contact />
         </div>
