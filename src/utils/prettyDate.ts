a80190004663dd3ba232d77950d26ccdeafc98aa
eirikhanasand <eirik.hanasand@gmail.com>
2025-05-12 06:10:44 +0200

    Custom date formatter

---
+++ src/utils/prettyDate.ts
@@ -0,0 +1,9 @@
+export function prettyDate(date: string) {
+    const d = new Date(date)
+    const day = d.getDate().toString().padStart(2, '0')
+    const month = (d.getMonth() + 1).toString().padStart(2, '0')
+    const year = d.getFullYear()
+    const hour = d.getHours().toString().padStart(2, '0')
+    const minute = d.getMinutes().toString().padStart(2, '0')
+    return `${day}.${month}.${year}, ${hour}:${minute}`
+}
