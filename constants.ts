c0840a98f1d4dbff771f048369cdf2ea75a16902
eirikhanasand <eirik.hanasand@gmail.com>
2025-05-12 03:56:06 +0200

    Renamed constants to article format

---
+++ constants.ts
@@ -0,0 +1,4 @@
+export const articles = 'https://raw.githubusercontent.com/eirikhanasand/hanasand/main/articles/'
+export const articles_commits = 'https://api.github.com/repos/eirikhanasand/hanasand/commits?path=articles/'
+export const articles_content = 'https://api.github.com/repos/eirikhanasand/hanasand/contents/articles'
+export const two_weeks_in_ms = 1209600000
