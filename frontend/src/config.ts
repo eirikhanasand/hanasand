c644f651f0bf0c1bdd360bc8fc89e7fff554cacc
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-13 03:25:40 +0200

    updated incorrect url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,7 @@ import packagejson from "../package.json"
 const config = {
     url: {
         api: 'https://api.hanasand.com/api',
-        cdn_ws: 'wss://api.hanasand.com/api',
+        cdn_ws: 'wss://cdn.hanasand.com/api',
         // cdn_ws: 'ws://localhost:8501/api',
         cdn: 'https://cdn.hanasand.com/api',
         // cdn: 'http://localhost:8501/api',
