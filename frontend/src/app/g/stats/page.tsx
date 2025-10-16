1133d164129570bdb4843139295d1f696c825a9d
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-17 01:09:26 +0200

    Improved rendering for small screens

--- frontend/src/app/g/stats/page.tsx
+++ frontend/src/app/g/stats/page.tsx
@@ -1,10 +1,8 @@
-import { ChartColumn, LinkIcon } from 'lucide-react'
-import Link from 'next/link'
 import LinkStatsPageClient from './pageClient'
 
 export default function Page() {
     return (
-        <div className='min-h-[93.5vh] w-full py-40 px-15 h-[30vh] md:h-full md:p-60 md:px-100 grid gap-2 place-items-center'>
+        <div className='min-h-[93.5vh] w-full py-40 px-15 h-[30vh] md:h-full md:p-[15rem] md:px-40 lg:px-100 grid gap-2 place-items-center'>
             <div className='grid w-full spawn rounded-lg overflow-hidden'>
                 <LinkStatsPageClient />
             </div>
