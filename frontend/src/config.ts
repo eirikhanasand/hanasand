f271d1ae45a0adb6462016a249d69ae3be2acb95
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-17 03:37:11 +0200

    Replaced dev urls

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,10 +2,10 @@ import packagejson from "../package.json"
 
 const config = {
     url: {
-        api: 'http://localhost:8080/api',
-        // api: 'https://api.hanasand.com/api',
-        api_ws: 'ws://localhost:8080/api',
-        // api_ws: 'wss://api.hanasand.com/api',
+        // api: 'http://localhost:8080/api',
+        api: 'https://api.hanasand.com/api',
+        // api_ws: 'ws://localhost:8080/api',
+        api_ws: 'wss://api.hanasand.com/api',
         cdn_ws: 'wss://cdn.hanasand.com/api',
         // cdn_ws: 'ws://localhost:8501/api',
         cdn: 'https://cdn.hanasand.com/api',
