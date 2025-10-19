af8e96a968c4678d4f4bb94bb8a8e778bcba69f5
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-19 02:17:15 +0200

    Added client side of logout handler

---
+++ frontend/src/components/logout/logoutClient.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import Notify from '../notify/notify'
+
+export default function LogoutClient({ logoutServer }: { logoutServer?: string }) {
+    const [logout, setLogout] = useState(Boolean(logoutServer) ?? false)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (logout) {
+            setTimeout(() => {
+                setLogout(false)
+                router.push('/')
+            }, 5000)
+        }
+    }, [logout])
+
+    if (!logout) {
+        return
+    }
+
+    return (
+        <div className='absolute top-0 left-0 p-10 grid place-items-center w-full'>
+            <Notify glow className='w-sm' color='bg-blue-800' fullWidth message='Successfully logged out. Welcome back later!' />
+        </div>
+    )
+}
