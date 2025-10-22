d4147f8355c6dbd33f5e57d69479fc1f15023b4d
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-22 19:12:51 +0200

    Added use client to client components

--- frontend/src/components/share/lock.tsx
+++ frontend/src/components/share/lock.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { getCookie } from '@/utils/cookies'
 import { lockShare } from '@/utils/share/lockShare'
 import { LockKeyhole, LockKeyholeOpen } from 'lucide-react'
--- frontend/src/utils/articles/putArticle.ts
+++ frontend/src/utils/articles/putArticle.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import config from '@/config'
 import { getCookie } from '@/utils/cookies'
 
