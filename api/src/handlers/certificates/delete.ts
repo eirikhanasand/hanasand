dee8cdea18e398b86f25a87c462aea5602f82242
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-07 01:07:14 +0100

    Fixed bugs

--- api/src/handlers/certificates/delete.ts
+++ api/src/handlers/certificates/delete.ts
@@ -3,8 +3,9 @@ import run from '#db'
 
 export default async function deleteCertificate(req: FastifyRequest, res: FastifyReply) {
     const { id } = req.params as { id: string }
-
-    if (!id) return res.status(400).send({ error: "No certificate id provided" })
+    if (!id) {
+        return res.status(400).send({ error: "No certificate id provided" })
+    }
 
     try {
         await run(`DELETE FROM certificates WHERE id = $1`, [id])
