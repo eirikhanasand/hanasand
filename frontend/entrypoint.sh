ec137ebacdc46286ada0bf5c0d6975f3ba782e7c
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-10 05:16:41 +0100

    Testing larger pool

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Starts varnish
-varnishd -a :3000 -f /etc/varnish/default.vcl -s malloc,512m &
+varnishd -a :3000 -f /etc/varnish/default.vcl -s malloc,512m -p thread_pool_min=50 -p thread_pool_max=4000 &
 
 # Starts API
 npm start
