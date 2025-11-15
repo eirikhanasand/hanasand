b4c23cede00357a5f1e8974dc1ca338ed2691c53
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-15 22:06:26 +0100

    Removed dead code

--- frontend/src/app/status/page.tsx
+++ frontend/src/app/status/page.tsx
@@ -4,14 +4,12 @@ import getBlocklist from '@/utils/traffic/getBlocklist'
 import getLogs from '@/utils/traffic/getLogs'
 import getDomains from '@/utils/traffic/getDomains'
 import getUAs from '@/utils/traffic/getUAs'
-import getIPs from '@/utils/traffic/getIPs'
 
 export default async function page() {
     const metrics = await getMetrics()
     const blocklist = await getBlocklist()
     const logs = await getLogs()
     const topDomainsUnparsed = await getDomains()
-    const topIPsUnparsed = await getIPs()
     const topUAsUnparsed = await getUAs()
     const topDomains = Array.isArray(topDomainsUnparsed) ? topDomainsUnparsed : []
     const topUAs = Array.isArray(topUAsUnparsed) ? topUAsUnparsed : []
