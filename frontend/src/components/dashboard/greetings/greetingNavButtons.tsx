6e1516a69ccdc1cfbff2f8cf0be10d8fafa4ddbe
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-16 23:17:29 +0100

    Added admin check before displaying management button

--- frontend/src/components/dashboard/greetings/greetingNavButtons.tsx
+++ frontend/src/components/dashboard/greetings/greetingNavButtons.tsx
@@ -1,15 +1,20 @@
 import { SquareChartGantt } from 'lucide-react'
+import { cookies } from 'next/headers'
 import Link from 'next/link'
 
-export default function GreetingNavButtons({ id }: { id: string }) {
+export default async function GreetingNavButtons({ id }: { id: string }) {
+    const Cookies = await cookies()
+    const roles: Role[] = JSON.parse(Cookies.get('roles')?.value || "[]")
+    const isAdmin = roles.some((role) => role.id.includes('admin'))
+
     return (
         <div className='flex md:hidden gap-2 w-full'>
-            <div className='grid h-fit px-2 py-1 outline-1 outline-dark w-full rounded-lg gap-2 group cursor-pointer'>
+            {isAdmin && <div className='grid h-fit px-2 py-1 outline-1 outline-dark w-full rounded-lg gap-2 group cursor-pointer'>
                 <Link href='/dashboard/management' className='flex justify-between w-full items-center gap-2'>
                     <SquareChartGantt className='w-5 h-5 group-hover:stroke-[#e25822]' />
                     <h1 className='font-semibold text-base self-center'>Management</h1>
                 </Link>
-            </div>
+            </div>}
             <div className='grid h-fit px-2 py-1 outline-1 outline-dark w-full rounded-lg gap-2'>
                 <Link href={`/profile/${id}`} className='flex justify-between w-full group cursor-pointer items-center'>
                     <div className='user-icon' />
