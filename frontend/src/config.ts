fab4f2d2e6a5d75327b7b86b4ab3e14f2c4bdaec
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-16 01:47:29 +0100

    Changed order

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,10 +2,10 @@ import packagejson from '../package.json'
 
 const config = {
     url: {
-        // api: 'http://localhost:8080/api',
         api: 'https://api.hanasand.com/api',
-        // api_ws: 'ws://localhost:8080/api/ws',
+        // api: 'http://localhost:8080/api',
         api_ws: 'wss://api.hanasand.com/api/ws',
+        // api_ws: 'ws://localhost:8080/api/ws',
         cdn_ws: 'wss://cdn.hanasand.com/api/ws',
         // cdn_ws: 'ws://localhost:8501/api/ws',
         cdn: 'https://cdn.hanasand.com/api',
