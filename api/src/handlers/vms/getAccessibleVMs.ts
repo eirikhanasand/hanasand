9d754ef044e8155f5fb4371b64a36437a495ace4
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-18 06:42:23 +0100

    Added auth check

--- api/src/handlers/vms/getAccessibleVMs.ts
+++ api/src/handlers/vms/getAccessibleVMs.ts
@@ -1,8 +1,13 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import run from '#db'
+import tokenWrapper from '#utils/auth/tokenWrapper.ts'
 
 export default async function getAccessibleVMs(req: FastifyRequest, res: FastifyReply) {
     const { user } = req.params as { user: string }
+    const { valid } = await tokenWrapper(req, res)
+    if (!valid) {
+        return res.status(401).send({ error: 'Unauthorized.' })
+    }
 
     if (!user) {
         return res.status(400).send({ error: "Missing user." })
@@ -15,7 +20,7 @@ export default async function getAccessibleVMs(req: FastifyRequest, res: Fastify
         )
 
         if (result.rows.length === 0) {
-            return res.status(404).send({ error: "No accessible VMs found for this user" })
+            return res.status(200).send([])
         }
 
         return res.send(result.rows)
