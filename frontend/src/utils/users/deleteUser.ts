a6ee787e23609e315ae8dcd4fa186af8e663ad8f
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-21 05:32:44 +0100

    Fixed issue causing incorrect user to be deleted

--- frontend/src/utils/users/deleteUser.ts
+++ frontend/src/utils/users/deleteUser.ts
@@ -1,26 +1,24 @@
-'use client'
-
 import config from '@/config'
 import { getCookie } from '../cookies/cookies'
 
 export default async function deleteUser(id: string): Promise<{ status: number, message: string }> {
     try {
         const token = getCookie('access_token')
-        const id = getCookie('id')
-        if (!token || !id) {
+        const userId = getCookie('id')
+        if (!token || !userId) {
             return {
                 status: 401,
                 message: 'Please log in to delete users.'
             }
         }
-        
+
         const controller = new AbortController()
         const timeout = setTimeout(() => controller.abort(), config.abortTimeout)
         const response = await fetch(`${config.url.api}/user/${id}`, {
             method: 'DELETE',
             headers: {
                 'Authorization': `Bearer ${token}`,
-                'id': id
+                'id': userId || ''
             },
             signal: controller.signal
         })
