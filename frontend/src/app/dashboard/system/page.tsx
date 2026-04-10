137689e1e926489afd75bdf42a1ec5f3e31a4ccd
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-11 00:03:04 +0200

    Added button to go to ai page

--- frontend/src/app/dashboard/system/page.tsx
+++ frontend/src/app/dashboard/system/page.tsx
@@ -5,6 +5,8 @@ import getSystemMetrics from '@/utils/vms/fetch/metrics/getSystemMetrics'
 import getVMsMetrics from '@/utils/vms/fetch/metrics/getVMsMetrics'
 import { redirect } from 'next/navigation'
 import getVMList from '@/utils/vms/fetch/getVMList'
+import Link from 'next/link'
+import { Sparkles } from 'lucide-react'
 
 export default async function page() {
     const Cookies = await cookies()
@@ -24,7 +26,15 @@ export default async function page() {
 
     return (
         <div className="h-full px-8 pb-4 md:px-16 lg:px-32 space-y-6">
-            <h1 className="font-semibold text-2xl text-bright/80">System Dashboard</h1>
+            <div className='flex justify-between'>
+                <h1 className="font-semibold text-2xl text-bright/80">System Dashboard</h1>
+                <div className='grid h-fit w-fit px-2 py-1 outline-1 outline-dark rounded-lg gap-2 group cursor-pointer'>
+                    <Link href='/dashboard/system/ai' className='flex justify-between px-9 items-center gap-2'>
+                        <Sparkles className='w-5 h-5 group-hover:stroke-orange-300' />
+                        <h1 className='font-semibold text-base self-center'>AI Metrics</h1>
+                    </Link>
+                </div>
+            </div>
             <SystemDashboard
                 system={system}
                 dockerContainers={dockerContainers}
