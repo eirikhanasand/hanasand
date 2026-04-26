be3bdfab0a0af4f7dcc2c14d53c2f5ef2bc7a933
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-26 07:51:41 +0200

    Send JSON body for test reruns

--- frontend/src/utils/test/rerunTest.ts
+++ frontend/src/utils/test/rerunTest.ts
@@ -3,6 +3,10 @@ import config from '@/config'
 export async function rerunTest(id: string | number) {
     const response = await fetch(`${config.url.api}/test/${id}/rerun`, {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
     })
 
     if (!response.ok) {
