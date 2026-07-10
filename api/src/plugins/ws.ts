af4de78210c184be46a40f89b5df4a202ae17a28
eirikhanasand <eirik.hanasand@gmail.com>
2026-07-10 14:45:22 +0200

    Limit browser worker log tails to unhealthy sessions

--- api/src/plugins/ws.ts
+++ api/src/plugins/ws.ts
@@ -326,7 +326,7 @@ function proxyEphemeralBrowserSocket(connection: WebSocket, id: string, route: '
             })
             upstream.on('close', (code, reason) => {
                 if (connection.readyState === WebSocket.OPEN) {
-                    if (!sawReady || !deliveredFrame || !sawTerminalMessage) void recordBrowserWorkerLogs(containerId, route, id)
+                    if (!sawReady || !deliveredFrame) void recordBrowserWorkerLogs(containerId, route, id)
                     void logBrowserRunClosure(id, sawReady, deliveredFrame, sawTerminalMessage, code, reason)
                     if (sawReady) {
                         void finishBrowserRun(id, 'ended')
